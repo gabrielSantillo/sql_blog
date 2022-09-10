3895e41d65e725e0c0ee041147384b566f7d63ae
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-10 17:11:57 -0600

    Fixed the UK problem by changing the name

--- blog.sql
+++ blog.sql
@@ -32,9 +32,10 @@ CREATE TABLE `blogger` (
   `age` smallint(5) unsigned DEFAULT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `blogger_un` (`username`,`email`),
+  UNIQUE KEY `blogger_un` (`username`),
+  UNIQUE KEY `blogger_un_email` (`email`),
   CONSTRAINT `blogger_check` CHECK (`age` >= 18 and `age` <= 120)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +44,7 @@ CREATE TABLE `blogger` (
 
 LOCK TABLES `blogger` WRITE;
 /*!40000 ALTER TABLE `blogger` DISABLE KEYS */;
-INSERT INTO `blogger` VALUES ('gasantillo','123','Hi, Im Gabriel','Gabriel','2000-01-01','gasantillo@gmail.com',26,1),('dansantillo','123','Hi, Im Daniel','Daniel','2001-01-01','dansantillo@gmail.com',20,4),('hani_five','123','Hi, Im Hani','Hani','2009-01-01','hani@gmail.com',21,5),('damasantillo','123','Hi, Im Damaris','Damaris','2002-01-01','damasantillo@gmail.com',50,6),('josesantillo','123','Hi, Im Jose','Jose','2003-01-01','josesantillo@gmail.com',52,7),('natysantillo','123','Hi, Im Natalia','Natalia','2004-01-01','natysantillo@gmail.com',37,8),('alex_one','123','Hi, Im Alex','Alex','2005-01-01','alex@gmail.com',30,9),('colleen_two','123','Hi, Im Collen','Colleen','2006-01-01','colleen@gmail.com',33,10),('james_three','123','Hi, Im James','James','2007-01-01','james@gmail.com',30,11),('satinder_four','123','Hi, Im Satinder','Satinder','2008-01-01','satinder@gmail.com',28,12);
+INSERT INTO `blogger` VALUES ('gasantillo','123','Hi, Im Gabriel','Gabriel','2000-01-01','gasantillo@gmail.com',26,1),('dansantillo','123','Hi, Im Daniel','Daniel','2001-01-01','dansantillo@gmail.com',20,4),('damasantillo','123','Hi, Im Damaris','Damaris','2002-01-01','damasantillo@gmail.com',50,6),('josesantillo','123','Hi, Im Jose','Jose','2003-01-01','josesantillo@gmail.com',52,7),('natysantillo','123','Hi, Im Natalia','Natalia','2004-01-01','natysantillo@gmail.com',37,8),('alex_one','123','Hi, Im Alex','Alex','2005-01-01','alex@gmail.com',30,9),('colleen_two','123','Hi, Im Collen','Colleen','2006-01-01','colleen@gmail.com',33,10),('james_three','123','Hi, Im James','James','2007-01-01','james@gmail.com',30,11),('satinder_four','123','Hi, Im Satinder','Satinder','2008-01-01','satinder@gmail.com',28,12),('_five','123','Hi, Im Hani','Hani','2009-01-01','hani@gmail.com',21,14);
 /*!40000 ALTER TABLE `blogger` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-10 16:44:09
+-- Dump completed on 2022-09-10 17:11:31
